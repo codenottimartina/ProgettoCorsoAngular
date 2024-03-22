fbbe95af10a6df5b73593812572ef5fcac5f261c
Martina <martina.codenotti2@gmail.com>
2024-03-22 12:15:52 +0100

    fino a lezione 312

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,13 @@ import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 const appRoutes: Routes  =  [
     {path: '', redirectTo: '/recipes', pathMatch: 'full' },
     /* pathMatch: 'full' assicura che il reindirizzamento avvenga solo quando il percorso corrente è esattamente
     uguale al percorso specificato (in questo caso, '/recipes').*/
-    {path: 'recipes', component: RecipesComponent, children: [
+    {path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
         {path: '', component: RecipeStartComponent},
         {path: 'new', component: RecipeEditComponent},
         {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
